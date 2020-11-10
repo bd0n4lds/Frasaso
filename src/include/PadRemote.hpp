98435145d8d3d3756c1ab9eb6263cf5af89d8505
Bri@n <bdonaldson@pm.me>
2020-11-09 21:23:23 -0500

    Added CMake

---
+++ src/include/PadRemote.hpp
@@ -0,0 +1,43 @@
+/**
+ * @file PadRemote.hpp
+ * @author Brian Donaldson <vuejs@johndoe.anonaddy.com>
+ * @version 1.0.2020-10-01
+ *
+ * @section DESCRIPTION
+ *
+ * Implementation of the parser. For Details see header.
+ *
+ * @section LICENSE
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License and a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#pragma once
+
+#ifndef PAD_REMOTE_HPP
+#define PAD_REMOTE_HPP
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+
+class PadRemote 
+{
+
+public:
+	PadRemote();
+	~PadRemote();
+
+private:
+
+};
+#endif // PAD_REMOTE_HPP
